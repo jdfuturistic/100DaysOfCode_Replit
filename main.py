682b3a9e52eab8b767519ee2bffdfc681ceae736
Joseph David <78045635+joseph004full@users.noreply.github.com>
2022-11-03 14:26:22 +0000

    Exercise day 1

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-print("Hola Replit")
-print("and how are you?")
-print("😎")
-
 print("""
-Here-s an exact
-    BIT
-      OF
-        TEXT
+Joseph
+September 20th 2019
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along for a minimun of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuse for not coding from the middle of a field!
+I'm feeling 🥳
+You can follow my progress at replit.com/@JosephValenzue1
 """)
